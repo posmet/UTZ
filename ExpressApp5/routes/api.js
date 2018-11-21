a41fdbcbc70758f09903a76e05f480d8a1c220ac
imikhayloff <opossum4eg@gmail.com>
2018-11-21 22:58:04 +0700

    refactoring

--- ExpressApp5/routes/api.js
+++ ExpressApp5/routes/api.js
@@ -1,23 +1,53 @@
-﻿var sql = require('mssql');
+﻿const sql = require('mssql');
 const pool = require('../boot/sql');
 const middleware = require('../services/Middleware');
 const authService = require('../services/Auth');
 const messageManager = require('../services/Message');
 
+const addwhere = function (conds) {
+  let sqlString = '';
+  if (conds.length > 0) {
+    sqlString = sqlString + ' where ';
+    sqlString = sqlString + conds.reduce(function (prev, curr) {
+      var Whr = prev;
+      if (prev != '')
+        Whr = Whr + ' and ';
+      Whr = Whr + curr.field;
+      switch (curr.cond) {
+        case 'eq':
+          Whr = Whr + " = '" + curr.value + "'";
+          break;
+        case 'neq':
+          Whr = Whr + " <> '" + curr.value + "'";
+          break;
+        case 'cn':
+          Whr = Whr + " Like '%" + curr.value + "%'";
+          break;
+        case 'ncn':
+          Whr = Whr + " = '" + curr.value + "'";
+          break;
+        case 'nl':
+          Whr = Whr + " = ''";
+          break;
+        case 'nnl':
+          Whr = Whr + " <> ''";
+          break;
+        case 'gt':
+          Whr = Whr + " > '" + curr.value + "'";
+          break;
+        case 'lt':
+          Whr = Whr + " < '" + curr.value + "'";
+          break;
+      }
+      return Whr;
+
+    }, "");
+    return sqlString;
+  }
+
+};
+
 module.exports = function (app) {
-    const config = {
-        user: 'online',
-        password: 'njkcnsq',
-        server: 'ga02.5dd.ru',
-        database: 'REQUEST',
-        connectionTimeout: 180000,
-        requestTimeout: 180000,
-        options: {
-            encrypt: false,
-            connectionTimeout: 180000,
-            requestTimeout: 180000
-        }
-    };
 
   app.get('/api/name', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
     const request = new sql.Request(pool);
@@ -207,256 +237,100 @@ module.exports = function (app) {
     res.json(rs.recordset);
   }));
 
-    app.get('/api/resultmtrxa/:pharmid/:grid', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
+  app.get('/api/resultmtrxa/:pharmid/:grid', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    let sqlString = 'SELECT matrix.*, ROUND(CalcVel*30000,0)/1000 as CalcVel30,Ph_Name from matrix inner join pharms on pharms.Ph_ID = matrix.Ph_ID';
+    if ((req.params.grid != 0) || (req.params.pharmid != 0)) {
+      sqlString = sqlString + ' where ';
+    }
+    if (req.params.pharmid != 0) {
+      sqlString = sqlString + 'matrix.ph_id = ' + req.params.pharmid;
+    }
+    if (req.params.grid != 0) {
+      if (req.params.pharmid != 0) {
+        sqlString = sqlString + ' and ';
+      }
+      sqlString = sqlString + ' Gr_ID = ' + req.params.grid;
+    }
+    console.log(sqlString);
+    const rs = await request.query(sqlString);
+    res.json(rs.recordset);
+  }));
 
-            var request = new sql.Request(connection);
+  app.post('/api/resultmtrxa/', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    let sqlString = 'SELECT * from matrix_cez';
+    if (req.body.grid != 0 || req.body.pharmid != 0) {
+      sqlString = sqlString + ' where ';
+    }
+    if (req.body.pharmid != 0) {
+      sqlString = sqlString + 'ph_id = ' + req.body.pharmid;
+    }
+    if (req.body.grid != 0) {
+      if (req.body.pharmid != 0) {
+        sqlString = sqlString + ' and ';
+      }
+      if (!Number.isInteger(req.body.grid)) {
+        req.body.grid = req.body.grid.replace(' ', '%');
+        sqlString = sqlString + " Gr_Name  Like '%" + req.body.grid + "%'";
+      } else {
+        sqlString = sqlString + ' Gr_ID = ' + req.body.grid;
+      }
+    }
+    console.log(sqlString);
+    const rs = await request.query(sqlString);
+    res.json(rs.recordset);
+  }));
 
-            var sqlString = 'SELECT matrix.*, ROUND(CalcVel*30000,0)/1000 as CalcVel30,Ph_Name from matrix inner join pharms on pharms.Ph_ID = matrix.Ph_ID';
-            if ((req.params.grid != 0) || (req.params.pharmid != 0)) {
-                sqlString = sqlString + ' where ';
-            }
-            if (req.params.pharmid != 0) {
-                sqlString = sqlString + 'matrix.ph_id = ' + req.params.pharmid 
-            }
-            if (req.params.grid != 0) {
-                if (req.params.pharmid != 0) {
-                    sqlString = sqlString + ' and ';
-                };
-                sqlString = sqlString + ' Gr_ID = ' + req.params.grid;
-            }
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
+  app.post('/api/resultmtrxf/', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const sqlString = "SELECT * from matrix_cez where Filial ='" + req.body.filial + "'";
+    console.log(sqlString);
+    const rs = await request.query(sqlString);
+    res.json(rs.recordset);
+  }));
 
-                if (err) { res.status(500).send(err); return; }
+  app.post('/api/updateph/', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const sqlString = "update pharms set d_d=" + req.body.D_D + ",d_a=" + req.body.D_A + ",d_t=" + req.body.D_T + ",kmin=" + req.body.Kmin + ",kmax=" + req.body.Kmax + ", Categories='" + req.body.Categories + "', graph = '" + req.body.graph + "', [over]=" + req.body.over+" where ph_id=" + req.body.Ph_ID;
+    console.log(sqlString);
+    await request.query(sqlString);
+    res.json(messageManager.buildSuccess());
+  }));
 
-                var count = rs;
-                res.status(200);
-                res.json(rs);
-            });
-        });
-    });
-    app.post('/api/resultmtrxa/', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-            console.log(req.body);
-            var request = new sql.Request(connection);
+  app.get('/api/acceptmx/:ph_id/:gr_id', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const grcode = req.params.gr_id;
+    const sqlString = "update matrix set Valid=1 where ph_id=" + req.params.ph_id + " and gr_id= " + grcode + "";
+    console.log(sqlString);
+    await request.query(sqlString);
+    res.json(messageManager.buildSuccess());
+  }));
 
-            var sqlString = 'SELECT * from matrix_cez';
-            if ((req.body.grid != 0) || (req.body.pharmid != 0)) {
-                sqlString = sqlString + ' where ';
-            }
-            if (req.body.pharmid != 0) {
-                sqlString = sqlString + 'ph_id = ' + req.body.pharmid
-            }
-            if (req.body.grid != 0) {
-                if (req.body.pharmid != 0) {
-                    sqlString = sqlString + ' and ';
-                };
-                if (!Number.isInteger(req.body.grid)) {
-                    req.body.grid = req.body.grid.replace(' ', '%');
-                    sqlString = sqlString + " Gr_Name  Like '%" + req.body.grid + "%'";
-                }
-                else
-                    sqlString = sqlString + ' Gr_ID = ' + req.body.grid;
-            }
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
+  app.get('/api/requests/:pharmid', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const sqlString = "SELECT * from Requests_v where Branch_Contractor_ID=" + req.params.pharmid + "  ORDER BY request_date desc";
+    console.log(sqlString);
+    const rs = await request.query(sqlString);
+    res.json(rs.recordset);
+  }));
 
-                if (err) { res.status(500).send(err); return; }
+  app.get('/api/reports/', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const sqlString = "select * from reports";
+    console.log(sqlString);
+    const rs = await request.query(sqlString);
+    res.json(rs.recordset);
+  }));
 
-                var count = rs;
-                res.status(200);
-                res.json(rs);
-            });
-        });
-    });
-    app.post('/api/resultmtrxf/', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-            console.log(req.body);
-            var request = new sql.Request(connection);
+  app.post('/api/reports/', authService.isAuthenticated(), middleware.asyncMiddleware(async (req, res) => {
+    const request = new sql.Request(pool);
+    const grcode = req.params.gr_id.replace("grp", "");
+    const sqlString = "update matrix set req=" + req.params.req + " where ph_id=" + req.params.ph_id + " and gr_id= " + grcode + "";
+    console.log(sqlString);
+    await request.query(sqlString);
+    res.json(messageManager.buildSuccess());
+  }));
 
-            var sqlString = "SELECT * from matrix_cez where Filial ='" + req.body.filial + "'";
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-
-                if (err) { res.status(500).send(err); return; }
-
-                var count = rs;
-                res.status(200);
-                res.json(rs);
-            });
-        });
-    });
-
-    app.post('/api/updateph/', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-
-            var request = new sql.Request(connection);
-            var grcode = req.params.gr_id;
-            //grcode = grcode.replace("grp", "");
-            var sqlString = "update pharms set d_d=" + req.body.D_D + ",d_a=" + req.body.D_A + ",d_t=" + req.body.D_T + ",kmin=" + req.body.Kmin + ",kmax=" + req.body.Kmax + ", Categories='" + req.body.Categories + "', graph = '" + req.body.graph + "', [over]=" + req.body.over+" where ph_id=" + req.body.Ph_ID;
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-                 
-                if (err) { res.status(500).send(err); return; }
-
-                //var count = rs;
-                res.status(200).send("ok");
-                res = "ok";
-            });
-        });
-
-    });
-   app.get('/api/acceptmx/:ph_id/:gr_id', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-            console.log('CONNECT');
-            var request = new sql.Request(connection);
-            var grcode = req.params.gr_id;
-            //grcode = grcode.replace("grp", "");
-            var sqlString = "update matrix set Valid=1 where ph_id=" + req.params.ph_id + " and gr_id= " + grcode + "";
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-
-                if (err) { res.status(500).send(err); return; }
-
-                //var count = rs;
-                res.status(200).send("ok");
-                //res = "ok";
-            });
-        });
-
-    });
-
-    app.get('/api/requests/:pharmid', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-
-            var request = new sql.Request(connection);
-            //           if (!req.user) { res.redirect('/'); return; };
-            //           if (!("username" in req.user)) { res.redirect('/'); return; };
-            var sqlString = "SELECT * from Requests_v where Branch_Contractor_ID=" + req.params.pharmid + "  ORDER BY request_date desc";
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-
-                if (err) { res.status(500).send(err); console.log(err); return; }
-
-                var count = rs;
-                res.status(200).json(rs);
-
-            });
-        });
-    });
-
-    app.get('/api/reports/', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-
-            var request = new sql.Request(connection);
-            var sqlString = "select * from reports";
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-
-                if (err) { res.status(500).send(err); return; }
-
-                //var count = rs;
-                res.status(200).json(rs);
-                res = "ok";
-            });
-        });
-
-    });
-    app.post('/api/reports/', function (req, res) {
-        var sql = require('mssql');
-        var connection = new sql.Connection(config);
-        connection.connect(function (err) {
-            if (err) { res.status(500).send(err); return; }
-
-            var request = new sql.Request(connection);
-            var grcode = req.params.gr_id;
-            grcode = grcode.replace("grp", "");
-            var sqlString = "update matrix set req=" + req.params.req + " where ph_id=" + req.params.ph_id + " and gr_id= " + grcode + "";
-            console.log(sqlString);
-            request.query(sqlString, function (err, rs) {
-                connection.close();
-
-                if (err) { res.status(500).send(err); return; }
-
-                //var count = rs;
-                res.status(200).send("ok");
-                res = "ok";
-            });
-        });
-
-    });
-    function addwhere(conds) {
-        var sqlString = '';
-        if (conds.length > 0) {
-            sqlString = sqlString + ' where ';
-            sqlString = sqlString + conds.reduce(function (prev, curr) {
-                var Whr = prev;
-                if (prev != '')
-                    Whr = Whr + ' and ';
-                Whr = Whr + curr.field;
-                switch (curr.cond) {
-                    case 'eq':
-                        Whr = Whr + " = '" + curr.value + "'";
-                        break;
-                    case 'neq':
-                        Whr = Whr + " <> '" + curr.value + "'";
-                        break;
-                    case 'cn':
-                        Whr = Whr + " Like '%" + curr.value + "%'";
-                        break;
-                    case 'ncn':
-                        Whr = Whr + " = '" + curr.value + "'";
-                        break;
-                    case 'nl':
-                        Whr = Whr + " = ''";
-                        break;
-                    case 'nnl':
-                        Whr = Whr + " <> ''";
-                        break;
-                    case 'gt':
-                        Whr = Whr + " > '" + curr.value + "'";
-                        break;
-                    case 'lt':
-                        Whr = Whr + " < '" + curr.value + "'";
-                        break;
-                }
-                return Whr;
-
-            }, "");
-            return sqlString;
-        }
-
-    };
 };
 
