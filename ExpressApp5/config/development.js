2c1a975e137cb4b75322eae50093b6a21ee0bfc8
Александр Посметный <posmet@yandex.ru>
2019-03-13 10:41:58 +0300

    е

--- ExpressApp5/config/development.js
+++ ExpressApp5/config/development.js
@@ -2,7 +2,7 @@ module.exports = {
   mssql: {
     user: 'online',
     password: 'njkcnsq',
-    server: 'ga02.5dd.ru',
-    database: 'REQUEST',
+    server: 'ga02.5dd.ru\\MSSQLSERVER1',
+    database: 'REQUEST'
   }
 };
