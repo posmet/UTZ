4300788b88ba9041ad21ca7e6699cc0b7d7148ac
imikhayloff <opossum4eg@gmail.com>
2019-03-20 19:43:53 +0700

    продажи30, 60

--- ExpressApp5/config/development.js
+++ ExpressApp5/config/development.js
@@ -1,8 +1,18 @@
 module.exports = {
-  mssql: {
+  /*mssql: {
     user: 'online',
     password: 'njkcnsq',
     server: 'ga02.5dd.ru\\MSSQLSERVER1',
     database: 'REQUEST'
+  },*/
+  mssql: {
+    user: 'sa',
+    password: '123456',
+    server: '192.168.1.37',
+    database: 'REQUEST',
+    options: {
+      instanceName: 'SQLEXPRESS',
+      database: 'REQUEST',
+    },
   }
 };
